86f82811715784525a6ececad1a8e67a744e5615
Santosh Rai <me.santosrai@gmail.com>
2023-12-07 04:02:31 +0000

    fix: add env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV = 'production'
 const nextConfig = {
     output: 'export',
    
+    basepath : isProd ? '/blog-starter-kit/' : '',
     // Optional: Change the output directory `out` -> `dist`
     distDir: 'dist',
 
